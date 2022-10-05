fcc4ba25e9ac0e1f5cfb961c2abcf166b2d76e9e
Frank Nguyen <phucnguyen.snow@gmail.com>
2022-10-05 15:03:13 +0700

    Update layout HTML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :error, :success
-  def after_sign_in_path_for(resource)
-    if request.referer == new_identity_session_url
-      super
-    else
-      stored_location_for(resource) || request.referer || root_path
-    end
-  end
 end
---
+++ app/helpers/option/workflow_type_option.rb
@@ -0,0 +1,8 @@
+module WorkflowTypeOption
+  extend AbstractOption
+  @hash_key = 'workflow_type_option'
+  @hash = {
+    'recruitment' => 'Recruitment',
+    'performance_review' => 'Performance Review',
+  }
+end
